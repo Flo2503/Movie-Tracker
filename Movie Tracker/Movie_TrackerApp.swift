19b3447cdeb063337962c6735799bd97ba6ffcba
Flo2503 <bruel.florent@gmail.com>
2022-08-23 19:39:22 +0200

    Initial Commit

---
+++ Movie Tracker/Movie_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_TrackerApp.swift
+//  Movie Tracker
+//
+//  Created by Florent on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Movie_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
